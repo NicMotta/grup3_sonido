8085af4b8f327e5250b37a5e76788a46d00ce3ac
NicMotta <34142261+NicMotta@users.noreply.github.com>
2020-08-22 17:07:25 -0300

    123

--- p5_sonido.js
+++ p5_sonido.js
@@ -14,7 +14,7 @@ let color_3;
 let color_4;
 
 function preload() {
-  
+
   sonido_1 = loadSound('assets/sonido_1.mp3');
   sonido_2 = loadSound('assets/sonido_2.mp3');
   sonido_3 = loadSound('assets/sonido_3.mp3');
@@ -23,40 +23,42 @@ function preload() {
 
 
 function setup() {
-  
+
   createCanvas(displayWidth -1, displayHeight -1);
-  
+
   sonido_1.loop();
   sonido_2.loop();
   sonido_3.loop();
   sonido_4.loop();
-  
-  
+
+
   background(80);
 }
 
 function draw() {
-  
-  
-  
+
+
+
   volumen_1 = map(mouseX, 0, displayWidth, 0, 1);
   volumen_2 = map(mouseY, 0, displayHeight, 0, 1);
   volumen_3 = map(mouseX, 0, displayWidth, 1, 0);
   volumen_4 = map(mouseY, 0, displayHeight, 1, 0);
- 
+
   sonido_1.setVolume((volumen_3 + volumen_4) /2);
   sonido_2.setVolume((volumen_1 + volumen_4) /2);
   sonido_3.setVolume((volumen_2 + volumen_3) /2);
-  sonido_4.setVolume((volumen_1 + volumen_2) /2);  
-  
+  sonido_4.setVolume((volumen_1 + volumen_2) /2);
+
   color_1 = map(volumen_1, 0, 1, 0, 255);
   color_2 = map(volumen_2, 0, 1, 0, 255);
   color_3 = map(volumen_3, 1, 0, 0, 255);
-  
+
   color_4 = map(volumen_4, 1, 0, 200, 800);
-   
+
   noFill();
   stroke(color_1, color_2, color_4, 50);
   line(width / 2, height / 2, mouseX, mouseY);
-  
+
 }
+
+function mousePressed() { getAudioContext().resume() }
